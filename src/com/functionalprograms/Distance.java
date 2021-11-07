56d6d238ca770bd96baa478e34c53e29ba1c1e2f
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-07 22:11:20 +0530

    calculating and printing Euclidean distance using Math.power function

--- src/com/functionalprograms/Distance.java
+++ src/com/functionalprograms/Distance.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class Distance {
 	public static void main(String[] args) {
-		
-		Scanner scan=new Scanner(System.in);
+
+		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter x coordinate : ");
 		int x = scan.nextInt();
 		System.out.print("Enter y coordinate : ");
 		int y = scan.nextInt();
-	
+
+		double distance = Math.pow(x * x + y * y, 0.5); // distance from point (x,y) to origin (0,0)
+		System.out.println("Euclidean distance= " + distance);
 	}
 
 }
